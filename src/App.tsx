73c5ae47bf638ee419a0a18aad3d00284bf638ad
Veronika Bulaeva <veronikabylaeva99@gmail.com>
2024-03-04 20:25:29 +0300

    tooltip without state

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import Table from "./components/table";
-import "./App.css";
-
-function App() {
-  return <Table />;
-}
-
-export default App;
--- src/requests/getData.ts
+++
@@ -1,6 +0,0 @@
-import { IResults } from "../types";
-
-export const getData = async (): Promise<IResults> => {
-  const result = await fetch("https://randomuser.me/api/?results=15");
-  return await result.json();
-};
